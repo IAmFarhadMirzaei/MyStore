f8a60194d903dcf54c6f42ccc4994836a83c5c66
IAmFarhadMirzaei <iamfarhadmirzaei@gmail.com>
2025-08-17 20:41:53 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
+#!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# بررسی وجود سوپریوزر و ساخت فقط در صورت نبودن
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get("SUPERUSER_USERNAME")
+email = os.environ.get("SUPERUSER_EMAIL")
+password = os.environ.get("SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print("Superuser created.")
+    else:
+        print("Superuser already exists.")
+else:
+    print("Superuser env variables are not fully set.")
+END
+
 python manage.py collectstatic --noinput
